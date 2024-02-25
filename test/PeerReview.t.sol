f178c082bd04f3e814c8c7c1035b04ee89e6ec9b
Kirill Igumenshchev <kirill.igum@gmail.com>
2024-02-24 23:45:58 -0700

    aider: Added a test to add authors to the `PeerReview` contract using provided accounts.
    # Aider chat conversation:
    
    USER: I added these *read-write* files: LICENSE
    
    ASSISTANT: How can I assist you with the LICENSE file or any other *read-write* files you've added?
    
    USER: add a test to add authors. use these accounts:     authors[0] = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8; // Anvil's local test account 1
        authors[1] = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC; // Anvil's local test account 2
    
    ASSISTANT: To add a test for adding authors to the `PeerReview` contract, we need to modify the `test/PeerReview.t.sol` file. This test will verify that authors can be successfully added to the `authors` array in the `PeerReview` contract using the provided accounts.
    
    Here's how we can implement this:
    
    1. Import the `PeerReview` contract at the beginning of the file.
    2. Define a new test function named `testAddAuthor`.
    3. In the `testAddAuthor` function, add the provided accounts as authors.
    4. Assert that the addresses in the `authors` array match the provided accounts.
    
    Here is the *SEARCH/REPLACE* block for adding the `testAddAuthor` function:
    
    test/PeerReview.t.sol
    ```solidity
    <<<<<<< SEARCH
    }
    =======
        function testAddAuthor() public {
            address expectedAuthor1 = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8; // Anvil's local test account 1
            address expectedAuthor2 = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC; // Anvil's local test account 2
    
            reviewProcess.addAuthor(expectedAuthor1);
            reviewProcess.addAuthor(expectedAuthor2);
    
            assertEq(reviewProcess.authors(0), expectedAuthor1);
            assertEq(reviewProcess.authors(1), expectedAuthor2);
        }
    >>>>>>> REPLACE
    ```
    
    This code snippet adds a new test function `testAddAuthor` at the end of the `PeerReview.t.sol` file. This function tests adding two authors to the `PeerReview` contract and asserts that their addresses are correctly stored in the `authors` array.

--- test/PeerReview.t.sol
+++ test/PeerReview.t.sol
@@ -15,4 +15,13 @@ contract PeerReviewTest is DSTest {
     function testInitialLicenseSetting() public {
         assertEq(reviewProcess.LICENSE(), expectedLicense);
     }
-}
+    function testAddAuthor() public {
+        address expectedAuthor1 = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8; // Anvil's local test account 1
+        address expectedAuthor2 = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC; // Anvil's local test account 2
+
+        reviewProcess.addAuthor(expectedAuthor1);
+        reviewProcess.addAuthor(expectedAuthor2);
+
+        assertEq(reviewProcess.authors(0), expectedAuthor1);
+        assertEq(reviewProcess.authors(1), expectedAuthor2);
+    }
